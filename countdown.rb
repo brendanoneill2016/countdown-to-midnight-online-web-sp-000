5e041ff8d6a303eb71058954dcd41e47d4dcf2de
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-11-07 20:51:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,9 +7,19 @@ def countdown(num)
   while number > 0 
   puts "#{number} SECOND(S)!"
   number -= 1 
-  sleep(0.99)
   end
   return "HAPPY NEW YEAR!"
 
 end
 
+def countdown_with_sleep
+    number = num 
+  
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
+  sleep(0.99)
+  end
+  return "HAPPY NEW YEAR!"
+end
+  
